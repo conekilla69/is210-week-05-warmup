ebcbbbad71ffcbcaf1fb3ede6022c4431370651d
conekilla69 <wrcfan@verizon.net>
2015-09-26 00:06:28 +0000

    Not able to properly set up task_01 as an init.  Not able to do imports properly.

---
+++ task_01/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""This is an init file"""
+
+
+from peanut import peanut
---
+++ task_01/peanut.py
@@ -0,0 +1,14 @@
+#1/usr/bin/env python
+#-*- coding:-utf8 -*-
+"""These will return specific values."""
+
+
+BUTTER = 'I am Truthy.'
+OIL = ''
+
+
+
+
+
+
+
